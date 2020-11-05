9d9f686ede8cabce2ede9b45e0aa77c0e4a0f0bd
beerwithstraw <agarwalp@tcd.ie>
2020-11-05 03:10:30 +0000

    Submit form for Join page

--- server/index.js
+++ server/index.js
@@ -10,6 +10,14 @@ const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
 
+io.on('connector', (socket) => {
+    console.log('We have a new connection!');
+
+    socket.on('disconnect', () => {
+        console.log('User has left!');
+    })
+});
+
 app.use(router); 
 
 server.listen(PORT, () => console.log(`Server started at PORT ${PORT}`));
