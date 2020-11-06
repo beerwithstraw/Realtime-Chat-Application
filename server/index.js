263c64c3088ae284e8b67ba81f216b7574d4a4de
beerwithstraw <agarwalp@tcd.ie>
2020-11-06 04:48:10 +0000

    Heroku & Netlify dependencies

--- server/index.js
+++ server/index.js
@@ -1,10 +1,9 @@
+const http = require('http');
 const express = require('express');
 const socketio = require('socket.io');
-const http = require('http');
+const cors = require('cors');
 
-const {addUser, removeUser, getUser, getUsersInRoom} = require('./users.js');
-
-const PORT = process.env.PORT || 5000;
+const { addUser, removeUser, getUser, getUsersInRoom } = require('./users');
 
 const router = require('./router');
 
@@ -12,33 +11,41 @@ const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
 
-io.on('connector', (socket) => {
-    console.log('We have a new connection!');
+app.use(cors());
+app.use(router);
 
-    socket.on('join', ({name, room}, callback) =>{
-        const {error, user} = addUser({ id: socket.id, name, room});
-        if (error) return callback(error);
+io.on('connect', (socket) => {
+  socket.on('join', ({ name, room }, callback) => {
+    const { error, user } = addUser({ id: socket.id, name, room });
 
-        socket.emit('message', {user: 'admin', text: `${user.name}, Welcome to the room ${user.room}`});
-        socket.broadcast.to(user.room).emit('message', { user: 'admin', text: `${user.name} has joined!`});
-        socket.join(user.room);
+    if(error) return callback(error);
 
-        callback();
+    socket.join(user.room);
 
-    });
+    socket.emit('message', { user: 'admin', text: `${user.name}, welcome to room ${user.room}.`});
+    socket.broadcast.to(user.room).emit('message', { user: 'admin', text: `${user.name} has joined!` });
 
-    socket.on('sendMessage', (message, callback) =>{
-        const user = getUser(socket.id);
+    io.to(user.room).emit('roomData', { room: user.room, users: getUsersInRoom(user.room) });
 
-        io.to(user.room).emit('message', {user: user.name, text: message});
-        callback();
-    });
+    callback();
+  });
 
-    socket.on('disconnect', () => {
-        console.log('User has left!');
-    })
+  socket.on('sendMessage', (message, callback) => {
+    const user = getUser(socket.id);
+
+    io.to(user.room).emit('message', { user: user.name, text: message });
+
+    callback();
+  });
+
+  socket.on('disconnect', () => {
+    const user = removeUser(socket.id);
+
+    if(user) {
+      io.to(user.room).emit('message', { user: 'Admin', text: `${user.name} has left.` });
+      io.to(user.room).emit('roomData', { room: user.room, users: getUsersInRoom(user.room)});
+    }
+  })
 });
 
-app.use(router); 
-
-server.listen(PORT, () => console.log(`Server started at PORT ${PORT}`));
+server.listen(process.env.PORT || 5000, () => console.log(`Server has started.`));
--- server/router.js
+++ server/router.js
@@ -1,8 +1,8 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-router.get('/', (req, res) => {
-    res.send('Server is Running now!')
+router.get("/", (req, res) => {
+  res.send("Server is up and running.").status(200);
 });
 
 module.exports = router;
